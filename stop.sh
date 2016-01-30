a80f7ded64331748fe124817a56ff9edd6f249f0
Matt Bierner <mattbierner@gmail.com>
2016-01-30 13:17:20 -0800

    Fixed stop trying to remove empty file

--- stop.sh
+++ stop.sh
@@ -2,5 +2,8 @@
 . config.sh
 PID_FILE=./target/universal/stage/RUNNING_PID
 
-test -f $PID_FILE && kill `cat $PID_FILE` && sleep 5;
-rm $PID_FILE;
+if [ -f $PID_FILE ]
+then
+	kill `cat $PID_FILE` && sleep 5;
+	rm $PID_FILE;
+fi
