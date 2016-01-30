185fa0792cf511fe1e1a3df5d03753a16dcac4f1
Matt Bierner <mattbierner@gmail.com>
2016-01-30 13:34:53 -0800

    Added blotre.sh script

--- config.sh
+++ config.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Global settings
+NAME=blotre
 PORT=9090
+TARGET_DIR=/home/blotre/target/universal
+APP_DIR=$TARGET_DIR/stage
+
+
+# Path to the file containing process ID
+PID_FILE=$APP_DIR/RUNNING_PID
--- stop.sh
+++ stop.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-. config.sh
-PID_FILE=./target/universal/stage/RUNNING_PID
-
-if [ -f $PID_FILE ]
-then
-	kill `cat $PID_FILE` && sleep 5;
-	rm $PID_FILE;
-fi
+./bin/blotre.sh stop
